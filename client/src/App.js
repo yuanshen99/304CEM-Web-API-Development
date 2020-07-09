410e9f0e6c9d74eeab47b70a29be2e18b3227dd3
Yuan Shen <sawyuanshen@hotmail.com>
2020-07-09 09:04:08 +0800

    trt

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,22 @@ class App extends Component {
     this.handleSubmit1 = this.handleSubmit1.bind(this);
     this.handleSubmit2 = this.handleSubmit2.bind(this);
   }
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+  this.callBackendAPI()
+    .then(res => this.setState({ data: res.express }))
+    .catch(err => console.log(err));
+}
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+callBackendAPI = async () => {
+  const response = await fetch('/express_backend');
+  const body = await response.json();
 
+  if (response.status !== 200) {
+    throw Error(body.message) 
+  }
+  return body;
+};
   getAllRecords = () => {
     axios
       .get('/getallrecords')
