357b42d5d200ae53bbbf2c5e63002dcdb19fd9a4
Yuan Shen <sawyuanshen@hotmail.com>
2020-07-09 09:43:56 +0800

    Add cool face API

--- client/src/App.js
+++ client/src/App.js
@@ -18,21 +18,9 @@ class App extends Component {
   }
   componentDidMount() {
     // Call our fetch function below once the component mounts
-  this.callBackendAPI()
-    .then(res => this.setState({ data: res.express }))
-    .catch(err => console.log(err));
     this.getAllRecords();
-}
-  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-callBackendAPI = async () => {
-  const response = await fetch('/express_backend');
-  const body = await response.json();
-
-  if (response.status !== 200) {
-    throw Error(body.message) 
   }
-  return body;
-};
+
   getAllRecords = () => {
     axios
       .get('/getallrecords')
@@ -47,9 +35,6 @@ callBackendAPI = async () => {
         console.log(error);
       });
   };
- /* componentDidMount() {
-    
-  }*/
 
   handleSubmit(e) {
     const query = `/getrecord?title=${this.input.value}`;
